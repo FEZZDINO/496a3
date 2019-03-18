e36955d6a708166b58a5aa0ed8186cf393ef8e48
Yuqi Zhang <yuqi8@ualberta.ca>
2019-03-18 14:53:50 -0600

    *x*xx*

--- simple_board.py
+++ simple_board.py
@@ -441,96 +441,61 @@ class SimpleGoBoard(object):
         if len(rtv) > 0:
             return rtv
         else:
-            return None
-        # else:
-        #     rtv = self._point_direction_check_another_win_two_gomoko(point, 1, color)
-        #     # print("1: ", rtv)
-        #     # check vertical
-        #     rtv += self._point_direction_check_another_win_two_gomoko(point, self.NS, color)
-        #     # print("self.NS: ", rtv)
-        #     # check y=x
-        #     rtv += self._point_direction_check_another_win_two_gomoko(point, self.NS + 1, color)
-        #     # print("self.NS + 1: ", rtv)
-        #     # check y=-x
-        #     rtv += self._point_direction_check_another_win_two_gomoko(point, self.NS - 1, color)
-        #     if len(rtv) > 0:
-        #         return rtv
-        #     else:
-        #         return None
+            print("enter")
+            rtv = self._point_direction_check_another_win_two_gomoko(point, 1, color)
+            # print("1: ", rtv)
+            # check vertical
+            rtv += self._point_direction_check_another_win_two_gomoko(point, self.NS, color)
+            # print("self.NS: ", rtv)
+            # check y=x
+            rtv += self._point_direction_check_another_win_two_gomoko(point, self.NS + 1, color)
+            # print("self.NS + 1: ", rtv)
+            # check y=-x
+            rtv += self._point_direction_check_another_win_two_gomoko(point, self.NS - 1, color)
+            print(rtv)
+            if len(rtv) > 0:
+                return rtv
+            else:
+                return None
 
 
-    # def _point_direction_check_another_win_two_gomoko(self, point, shift, color):
-    #
-    #     """ *x*xx* """
-    #     color_count = 1
-    #     first_empty_count = 0
-    #     second_empty_count = 0
-    #     empty_flag = False
-    #     good_empty_point = None
-    #     first_good_empty_point = None
-    #     second_good_empty_point = None
-    #     color_continuous = True
-    #     d1 = shift
-    #     p1 = point
-    #     d2 = -shift
-    #     p2 = point
-    #
-    #     while True: # count color
-    #         p1 = p1 + d1
-    #         p2 = p2 + d2
-    #
-    #         if self.board[p1] == color and color_continuous:
-    #             color_count = color_count + 1
-    #         elif self.board[p1] == EMPTY:
-    #             color_continuous = False
-    #             first_empty_count += 1
-    #             if first_empty_count >= 2:
-    #                 break
-    #             elif first_empty_count == 1:
-    #                 first_good_empty_point = p1
-    #         else:
-    #             break
-    #
-    #     if first_empty_count == 0:
-    #         return []
-    #     elif first_empty_count == 1:
-    #         first_good_empty_point = None
-    #
-    #     d = -d
-    #     p = point
-    #     color_continuous = False
-    #     while True: # count color
-    #         p = p + d
-    #         if self.board[p] == color and color_continuous:
-    #             color_count = color_count + 1
-    #         elif self.board[p] == EMPTY:
-    #             color_continuous = False
-    #             second_empty_count += 1
-    #             if second_empty_count >= 2:
-    #                 break
-    #             elif second_empty_count == 1:
-    #                 second_good_empty_point = p
-    #         else:
-    #             break
-    #
-    #     if second_empty_count == 0:
-    #         return []
-    #     elif second_empty_count == 1:
-    #         second_good_empty_point = None
-    #
-    #     # assert count <= 5
-    #     if (first_empty_count + second_empty_count) >= 3:
-    #         empty_flag = True
-    #
-    #     if color_count >= 3 and empty_flag:
-    #         if first_good_empty_point and second_good_empty_point:
-    #             return [first_good_empty_point, second_good_empty_point]
-    #         elif first_good_empty_point:
-    #             return [first_good_empty_point]
-    #         elif second_good_empty_point:
-    #             return [second_good_empty_point]
-    #     else:
-    #         return []
+    def _point_direction_check_another_win_two_gomoko(self, point, shift, color):
+
+        """ *x*xx* """
+        rt_list = []
+        first = None
+        second = None
+
+        d1 = shift
+        p1 = point
+        d2 = -shift
+        p2 = point
+        length = len(self.board)
+
+        p1 += d1
+        if self.board[p1] == EMPTY:
+            first = p1
+            p1 += d1
+            if self.board[p1] == color:
+                p1 += d1
+                if self.board[p1] == color:
+                    p1 += d1
+                    if self.board[p1] == EMPTY:
+                        if self.board[point + d2] == EMPTY:
+                            rt_list.append(first)
+        p2 += d2
+        if self.board[p2] == EMPTY:
+            second = p2
+            p2 += d2
+            if self.board[p2] == color:
+                p2 += d2
+                if self.board[p2] == color:
+                    p2 += d2
+                    if self.board[p2] == EMPTY:
+                        if self.board[point + d1] == EMPTY:
+                            rt_list.append(second)
+
+        return rt_list
 
     def _point_direction_check_win_win_two_gomoko(self, point, shift, color):
         """
